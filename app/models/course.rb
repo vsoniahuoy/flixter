ee149e2254f2e889ea5edc321a329df0c1a4ef5b
Sonia Huoy <vsoniahuoy@gmail.com>
2019-07-08 21:00:09 +0000

    push updated codes

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,12 @@
 class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
-  has_many :photos
+  mount_uploader :image, ImageUploader
+
 
   validates :title, presence: true
   validates :description, presence: true
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+
 
 end
