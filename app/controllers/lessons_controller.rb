4781c33e0fc1fa4fa58855fd1ba7752a0d20f678
Sonia Huoy <vsoniahuoy@gmail.com>
2019-07-14 17:47:21 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
   def show
-    # @photo = Photo.new
   end
 
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.user != current_user.enrolled_in?(current_lesson)
+      redirect_to course_path(current_lesson), alert: 'You must enroll before viewing the lesson'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
