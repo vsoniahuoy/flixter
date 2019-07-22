79418053417f0f1d26c3ef36efa0b0ee3c272b25
Sonia Huoy <vsoniahuoy@gmail.com>
2019-07-22 01:03:37 +0000

    New section from modal

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :dashboard, only: [:show]
   root 'static_pages#index'
- resources :courses, only: [:index, :show] do
-  resources :enrollments, only: :create
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
 end
-  resources :lessons, only: [:show]
-  namespace :instructor do
-    resources :lessons, only: [:update]
-    resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
-    end
-    resources :sections, only: [:update]
-    resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
-    end
+resources :lessons, only: [:show]
+namespace :instructor do
+  resources :lessons, only: [:update]
+  resources :sections, only: [:update] do
+    resources :lessons, only: [:new, :create]
+  end
+  # resources :sections, only: [:update]
+  resources :courses, only: [:new, :create, :show] do
+    resources :sections, only: [:create]
+  end
   end
 end
